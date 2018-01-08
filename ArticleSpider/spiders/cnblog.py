bae989e750fb58eda2c28a3fc12717fd665a0046
dotbalo <727585266@qq.com>
2018-01-08 12:20:12 +0800

    some change

--- ArticleSpider/spiders/cnblog.py
+++ ArticleSpider/spiders/cnblog.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders.splashcrawl import Rule,CrawlSpider
+# from scrapy.spiders.splashcrawl import Rule,CrawlSpider
 from scrapy_redis.spiders import RedisSpider
 from ArticleSpider.items import CNBlogItem
-# from scrapy.spiders import Rule
+from scrapy.spiders import Rule,CrawlSpider
 
 
 class cnblogSpider(CrawlSpider,RedisSpider):
     name = 'cnblog'
-    allowed_domains = ['cnblogs.com']
+    allowed_domains = ['www.cnblogs.com']
     redis_key = "cnblog:start_urls"
     url_list = []
-
+    deny = ['www.cnblogs.com/new/']
     rules = (
         # 获取网站分类
         # Rule(LinkExtractor(allow=r'/cate/.*/$'), follow=True),
         # 匹配博主
-        Rule(LinkExtractor(allow=r'/\w+/$'), follow=True),
+        Rule(LinkExtractor(allow=r'/\w+/$',deny_domains=deny), follow=True),
         # 匹配下页
-        Rule(LinkExtractor(allow=r'/sitehome/p/\d+$'), follow=True),
-        Rule(LinkExtractor(allow=r'/cate/all/($|\d+)'), follow=True),
+        Rule(LinkExtractor(allow=r'/sitehome/p/\d+$',deny_domains=deny), follow=True),
+        Rule(LinkExtractor(allow=r'/cate/all/($|\d+)',deny_domains=deny), follow=True),
         # 匹配博客
-        Rule(LinkExtractor(allow=r'.*/p/.+\.html$'), callback="parse_job", follow=True),
+        Rule(LinkExtractor(allow=r'.*/p/.+\.html$',deny_domains=deny), callback="parse_job", follow=True),
+        # Rule(LinkExtractor(allow=r'.*/page/\d+/$',deny_domains=deny), callback="parse_job", follow=True),
 
     )
 
@@ -33,7 +34,7 @@ class cnblogSpider(CrawlSpider,RedisSpider):
         # author_total_comment =  response.css('#blog_stats>span:last-child::text').re_first('(\d+)')
         title = response.css('#cb_post_title_url::text').extract_first()
         content = response.css('#cnblogs_post_body').extract_first()
-        post_date = response.css('#post-date::text').extract_first().split()[0]
+        post_date = response.css('#post-date::text').extract_first()
         class_ification = ",".join(response.css("#BlogPostCategory a::text").extract())
         tag = ",".join(response.css("#EntryTag a::text").extract())
         url = response.url
